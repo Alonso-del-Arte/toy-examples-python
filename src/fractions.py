0d56b9721d85fc09c47049f926a0f8cf5c070b61
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-29 14:58:04 -0400

    Passing __str__( ) for integer

--- src/fractions.py
+++ src/fractions.py
@@ -13,6 +13,8 @@ class Fraction :
         self.denominator = denom // gcd
 
     def __str__(self) :
+        if (self.denominator == 1) :
+            return str(self.numerator)
         return str(self.numerator) + "/" + str(self.denominator)
     
     # TODO: Write tests for this
