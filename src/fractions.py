a38e0fcf03eccb77c0a6b300bb4ac84047018f03
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-28 23:59:10 -0400

    Passing __str__( ) test

--- src/fractions.py
+++ src/fractions.py
@@ -11,3 +11,6 @@ class Fraction :
     def __init__(self, numer, denom) :
         self.numerator = numer
         self.denominator = denom
+
+    def __str__(self) :
+        return str(self.numerator) + "/" + str(self.denominator)
